44153d4711f31d58dd3a3fa6f3aa9407150bc79e
Linux *User <70212520+nexus1203@users.noreply.github.com>
2023-12-20 14:19:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='TSAlign',
-    version='0.1.0',
-    author='Your Name',
+    version='1.0.0',
+    author='Nexus1203',
     author_email='your.email@example.com',
     description='A fast library to align two 1D time-series using FFT based convolution.',
     long_description=open('README.md').read(),
